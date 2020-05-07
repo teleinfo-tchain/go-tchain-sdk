48abaf6b24a19776d45844c510add6d9ae549094
xufeng ma <1005206460@qq.com>
2020-05-07 16:41:09 +0800

    删除secp256k1 c代码

--- crypto/signaturesecp256k1.go
+++ crypto/signaturesecp256k1.go
@@ -14,33 +14,38 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-bif library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build !nacl,!js,cgo
 
 package crypto
 
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"errors"
 	"fmt"
+	"math/big"
 
-	"github.com/bif/bif-sdk-go/common/math"
-	"github.com/bif/bif-sdk-go/crypto/secp256k1"
+	"github.com/btcsuite/btcd/btcec"
 )
 
 // Ecrecover returns the uncompressed public key that created the given signature.
 func EcrecoverBtc(hash, sig []byte) ([]byte, error) {
-	return secp256k1.RecoverPubkey(hash, sig)
+	pub, err := SigToPubBtc(hash, sig)
+	if err != nil {
+		return nil, err
+	}
+	bytes := (*btcec.PublicKey)(pub).SerializeUncompressed()
+	return bytes, err
 }
 
 // SigToPub returns the public key that created the given signature.
 func SigToPubBtc(hash, sig []byte) (*ecdsa.PublicKey, error) {
-	s, err := EcrecoverBtc(hash, sig)
-	if err != nil {
-		return nil, err
-	}
+	// Convert to btcec input format with 'recovery id' v at the beginning.
+	btcsig := make([]byte, 65)
+	btcsig[0] = sig[64] + 27
+	copy(btcsig[1:], sig)
 
-	x, y := elliptic.Unmarshal(S256Btc(), s)
-	return &ecdsa.PublicKey{Curve: S256Btc(), X: x, Y: y}, nil
+	pub, _, err := btcec.RecoverCompact(btcec.S256(), btcsig, hash)
+	return (*ecdsa.PublicKey)(pub), err
 }
 
 // Sign calculates an ECDSA signature.
@@ -51,38 +56,62 @@ func SigToPubBtc(hash, sig []byte) (*ecdsa.PublicKey, error) {
 // solution is to hash any input before calculating the signature.
 //
 // The produced signature is in the [R || S || V] format where V is 0 or 1.
-// hash = Hash(M) 32  hash = Hash(M) + uid + 标志位
-func SignBtc(hash []byte, prv *ecdsa.PrivateKey) (sig []byte, err error) {
+func SignBtc(hash []byte, prv *ecdsa.PrivateKey) ([]byte, error) {
 	if len(hash) != 32 {
 		return nil, fmt.Errorf("hash is required to be exactly 32 bytes (%d)", len(hash))
 	}
-	seckey := math.PaddedBigBytes(prv.D, prv.Params().BitSize/8)
-	defer zeroBytes(seckey)
-	return secp256k1.Sign(hash, seckey)
+	if prv.Curve != btcec.S256() {
+		return nil, fmt.Errorf("private key curve is not secp256k1")
+	}
+	sig, err := btcec.SignCompact(btcec.S256(), (*btcec.PrivateKey)(prv), hash, false)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert to Ethereum signature format with 'recovery id' v at the end.
+	v := sig[0] - 27
+	copy(sig, sig[1:])
+	sig[64] = v
+	return sig, nil
 }
 
 // VerifySignature checks that the given public key created signature over hash.
 // The public key should be in compressed (33 bytes) or uncompressed (65 bytes) format.
 // The signature should have the 64 byte [R || S] format.
 func VerifySignatureBtc(pubkey, hash, signature []byte) bool {
-	return secp256k1.VerifySignature(pubkey, hash, signature)
+	if len(signature) != 64 {
+		return false
+	}
+	sig := &btcec.Signature{R: new(big.Int).SetBytes(signature[:32]), S: new(big.Int).SetBytes(signature[32:])}
+	key, err := btcec.ParsePubKey(pubkey, btcec.S256())
+	if err != nil {
+		return false
+	}
+	// Reject malleable signatures. libsecp256k1 does this check but btcec doesn't.
+	if sig.S.Cmp(secp256k1halfN) > 0 {
+		return false
+	}
+	return sig.Verify(hash, key)
 }
 
 // DecompressPubkey parses a public key in the 33-byte compressed format.
 func DecompressPubkeyBtc(pubkey []byte) (*ecdsa.PublicKey, error) {
-	x, y := secp256k1.DecompressPubkey(pubkey)
-	if x == nil {
-		return nil, fmt.Errorf("invalid public key")
+	if len(pubkey) != 33 {
+		return nil, errors.New("invalid compressed public key length")
 	}
-	return &ecdsa.PublicKey{X: x, Y: y, Curve: S256Btc()}, nil
+	key, err := btcec.ParsePubKey(pubkey, btcec.S256())
+	if err != nil {
+		return nil, err
+	}
+	return key.ToECDSA(), nil
 }
 
 // CompressPubkey encodes a public key to the 33-byte compressed format.
 func CompressPubkeyBtc(pubkey *ecdsa.PublicKey) []byte {
-	return secp256k1.CompressPubkey(pubkey.X, pubkey.Y)
+	return (*btcec.PublicKey)(pubkey).SerializeCompressed()
 }
 
 // S256 returns an instance of the secp256k1 curve.
 func S256Btc() elliptic.Curve {
-	return secp256k1.S256()
+	return btcec.S256()
 }
