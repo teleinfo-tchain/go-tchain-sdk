3a8101e51ce087ae58da40f3356c868d2edc61d7
yashuoJin <j203025923@163.com>
2020-06-22 22:29:57 +0800

    add test-data-struct

---
+++ utils/test-data-struct.go
@@ -0,0 +1,38 @@
+package utils
+
+type test1 struct {
+	input        string
+	want         interface{}
+	wantErr      error // if set, decoding must fail on any platform
+	wantErr32bit error // if set, decoding must fail on 32bit platforms (used for Uint tests)
+}
+
+type test2 struct {
+	input1 string
+	input2 int
+	input3 []string
+	want  interface{}
+}
+
+
+type test3 struct {
+	input        [] byte
+	want         interface{}
+	wantErr      error // if set, decoding must fail on any platform
+	wantErr32bit error // if set, decoding must fail on 32bit platforms (used for Uint tests)
+}
+
+type test4 struct {
+	input        interface{}
+	want         interface{}
+	wantErr      error // if set, decoding must fail on any platform
+	wantErr32bit error // if set, decoding must fail on 32bit platforms (used for Uint tests)
+}
+
+type test5 struct {
+	inputOne     string
+	inputTwo     []string
+	want         interface{}
+	wantErr      error // if set, decoding must fail on any platform
+	wantErr32bit error // if set, decoding must fail on 32bit platforms (used for Uint tests)
+}
