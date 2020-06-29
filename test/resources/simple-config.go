e56c54860565acad826ab80cc168bbbf263ad1a9
yashuoJin <j203025923@163.com>
2020-06-29 14:04:44 +0800

    add simple config

---
+++ test/resources/simple-config.go
@@ -0,0 +1,8 @@
+package resources
+
+const (
+	IP string = "172.20.3.21"
+	Port string = "44002"
+	CoinBase string = "did:bid:6cc796b8d6e2fbebc9b3cf9e"
+	AddressTwo string = "did:bid:c117c1794fc7a27bd301ae52"
+)
