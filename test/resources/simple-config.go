e963594907b0e9c4001aa755dfe3f5cd8ae16dce
yashuoJin <j203025923@163.com>
2020-07-02 20:05:13 +0800

    change config

--- test/resources/simple-config.go
+++ test/resources/simple-config.go
@@ -3,6 +3,9 @@ package resources
 const (
 	IP string = "172.20.3.21"
 	Port string = "44002"
-	CoinBase string = "did:bid:6cc796b8d6e2fbebc9b3cf9e"
+	CoinBase string = "did:bid:c935bd29a90fbeea87badf3e"
+	CoinBasePriKey string = "296450657cf696cf3175b462415a84a8969dd4219841741690e85f770e4654d7"
 	AddressTwo string = "did:bid:c117c1794fc7a27bd301ae52"
+	AddressSM2 string = "did:bid:7368eb1b26408f44721e20bd"
+	AddressPriKey string = "19af4f854306a84084de393d09addd088a411ae011fcfd7e9a906525ff81226d"
 )
