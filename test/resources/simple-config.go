5bd10fc9075f4a80f1e686e80cf4bcb114a73b1c
yashuoJin <j203025923@163.com>
2020-07-10 11:01:26 +0800

    modify resources

--- test/resources/simple-config.go
+++ test/resources/simple-config.go
@@ -1,11 +1,11 @@
 package resources
 
 const (
-	IP string = "172.20.3.21"
-	Port string = "44002"
-	CoinBase string = "did:bid:c935bd29a90fbeea87badf3e"
+	IP             string = "172.17.6.53"
+	Port           string = "44002"
+	CoinBase       string = "did:bid:c935bd29a90fbeea87badf3e"
 	CoinBasePriKey string = "296450657cf696cf3175b462415a84a8969dd4219841741690e85f770e4654d7"
-	AddressTwo string = "did:bid:c117c1794fc7a27bd301ae52"
-	AddressSM2 string = "did:bid:7368eb1b26408f44721e20bd"
-	AddressPriKey string = "19af4f854306a84084de393d09addd088a411ae011fcfd7e9a906525ff81226d"
+	AddressTwo     string = "did:bid:c117c1794fc7a27bd301ae52"
+	AddressSM2     string = "did:bid:7368eb1b26408f44721e20bd"
+	AddressPriKey  string = "19af4f854306a84084de393d09addd088a411ae011fcfd7e9a906525ff81226d"
 )
