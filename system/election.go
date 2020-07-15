dd48a27e7d3f6a1b4dd6fdff0f575febc9d02ae8
yashuoJin <j203025923@163.com>
2020-07-15 14:36:57 +0800

    测试没有问题，部分接口的理解有问题

---
+++ system/election.go
@@ -0,0 +1,404 @@
+package system
+
+import (
+	"errors"
+	"github.com/bif/bif-sdk-go/abi"
+	"github.com/bif/bif-sdk-go/common"
+	"github.com/bif/bif-sdk-go/common/hexutil"
+	"github.com/bif/bif-sdk-go/complex/types"
+	"math/big"
+	"strings"
+)
+
+const (
+	ElectionContractAddr      = "did:bid:000000000000000000000009"
+	//VoteLimit         = 64
+	OneDay            = int64(24) * 3600
+	VoteOrProxyOneDay = OneDay
+	//VoteOrProxyOneDay = 60
+	//oneWeek = OneDay * 7
+	//OneYear = OneDay * 365 //代币增发周期 一年
+	year    = 1559318400   // 2019-06-01 00:00:00
+)
+
+var (
+	ErrCandiNameLenInvalid    = errors.New("the length of candidate's name should between [4, 20]")
+	ErrCandiUrlLenInvalid     = errors.New("the length of candidate's website url should between [6, 60]")
+	ErrCandiNameInvalid       = errors.New("candidate's name should consist of digits and lowercase letters")
+	ErrCandiInfoDup           = errors.New("candidate's name, website url or node url is duplicated with a registered candidate")
+	ErrCandiAlreadyRegistered = errors.New("candidate is already registered")
+	ErrPeerNotTrust           = errors.New("peer is not apply trust")
+)
+
+var (
+	nowTimeStamp = big.NewInt(year)
+
+	// 投票周期
+	unStakePeriod   = big.NewInt(VoteOrProxyOneDay)
+	baseBounty      = big.NewInt(0).Mul(big.NewInt(1e+18), big.NewInt(1000))
+	restTotalBounty = big.NewInt(0).Mul(big.NewInt(1e18), big.NewInt(1e9))
+
+	//代币增量 初始发行量的5%
+	tokenAdd = big.NewInt(0).Div(restTotalBounty, big.NewInt(20))
+
+)
+
+const ElectionAbiJSON = `[
+{"constant": false,"name":"registerWitness","inputs":[{"name":"nodeUrl","type":"string"},{"name":"website","type":"string"},{"name":"name","type":"string"}],"outputs":[],"type":"function"}, 
+{"constant": false,"name":"unregisterWitness","inputs":[],"outputs":[],"type":"function"},
+{"constant": false,"name":"voteWitnesses","inputs":[{"name":"candidate","type":"string"}],"outputs":[],"type":"function"},
+{"constant": false,"name":"cancelVote","inputs":[],"outputs":[],"type":"function"},
+{"constant": false,"name":"startProxy","inputs":[],"outputs":[],"type":"function"},
+{"constant": false,"name":"stopProxy","inputs":[],"outputs":[],"type":"function"},
+{"constant": false,"name":"cancelProxy","inputs":[],"outputs":[],"type":"function"},
+{"constant": false,"name":"setProxy","inputs":[{"name":"proxy","type":"string"}],"outputs":[],"type":"function"},
+{"constant": false,"name":"stake","inputs":[{"name":"stakeCount","type":"uint256"}],"outputs":[],"type":"function"},
+{"constant": false,"name":"unStake","inputs":[],"outputs":[],"type":"function"},
+{"constant": false,"name":"extractOwnBounty","inputs":[],"outputs":[],"type":"function"},
+{"anonymous":false,"inputs":[{"indexed":false,"name":"methodName","type":"string"},{"indexed":false,"name":"status","type":"uint32"},{"indexed":false,"name":"reason","type":"string"},{"indexed":false,"name":"time","type":"uint256"}],"name":"electEvent","type":"event"},
+{"constant": false,"name":"issueAdditionalBounty","inputs":[],"outputs":[],"type":"function"},
+{"constant": true,"name":"queryCandidates","inputs":[{"name":"candidateAddress","type":"string"}],"outputs":[{"name":"owner","type":"string"},{"name":"voteCount","type":"uint64"},{"name":"active","type":"bool"},{"name":"url","type":"string"},{"name":"totalBounty","type":"uint64"},{"name":"extractedBounty","type":"uint64"},{"name":"lastExtractTime","type":"uint64"},{"name":"website","type":"string"},{"name":"name","type":"string"}],"type":"function"},
+{"constant": true,"name":"queryAllCandidates","inputs":[],"outputs":[{"name":"listNum","type":"uint64"},{"name":"candidateInfoList","type":"string"}],"type":"function"},
+{"constant": true,"name":"queryVoter","inputs":[{"name":"voterAddress","type":"string"}],"outputs":[{"name":"owner","type":"string"},{"name":"isProxy","type":"bool"},{"name":"proxyVoteCount","type":"uint64"},{"name":"proxy","type":"string"},{"name":"lastVoteCount","type":"uint64"},{"name":"timestamp","type":"uint64"},{"name":"voteCandidatesList","type":"string"}],"type":"function"},
+{"constant": true,"name":"queryStake","inputs":[{"name":"voterAddress","type":"string"}],"outputs":[{"name":"owner","type":"string"},{"name":"stakeCount","type":"uint64"},{"name":"timestamp","type":"uint64"}],"type":"function"},
+{"constant": true,"name":"queryVoterList","inputs":[{"name":"candidateAddress","type":"string"}],"outputs":[{"name":"candidateAddress","type":"string"},{"name":"voterNum","type":"uint64"},{"name":"voterList","type":"string"}],"type":"function"}
+]`
+
+
+type Election struct {
+	super *System
+	abi   abi.ABI
+}
+
+type RegisterWitness struct {
+	NodeUrl string
+	Website string
+	Name string
+}
+
+type candidateInfo struct {
+	Owner string
+	VoteCount uint64
+	Active bool
+	Url string
+	TotalBounty uint64
+	ExtractedBounty uint64
+	LastExtractTime uint64
+	Website string
+	Name string
+}
+
+type candidatesParsing struct {
+	ListNum uint64
+	CandidateInfoList string
+}
+
+type candidates struct {
+	Num uint64
+	CandidatesList []string
+}
+type voter struct {
+	Owner string
+	IsProxy bool
+	ProxyVoteCount uint64
+	Proxy string
+	LastVoteCount uint64
+	Timestamp uint64
+	VoteCandidatesList string
+}
+
+type votersParsing struct {
+	CandidateAddress string
+	VoterNum uint64
+	VoterList string
+}
+
+type Voters struct {
+	CandidateAddress string
+	VoterNum uint64
+	VotersLi []string
+}
+
+type stake struct {
+	Owner string
+	StakeCount uint64
+	Timestamp uint64
+}
+
+func (system *System) NewElection() *Election {
+	parsedAbi, _ := abi.JSON(strings.NewReader(ElectionAbiJSON))
+
+	election := new(Election)
+	election.abi = parsedAbi
+	election.super = system
+	return election
+}
+
+//"registerWitness","inputs":[{"name":"nodeUrl","type":"string"},{"name":"website","type":"string"},{"name":"name","type":"string"}],"outputs":[]
+func(election *Election) RegisterWitness(from common.Address, witness *RegisterWitness) (string, error){
+	//encode
+	// witness is a struct we need to use the components.
+	var values []interface{}
+	values = election.super.StructToInterface(*witness,values)
+	inputEncode, err := election.abi.Pack("registerWitness", values...)
+	if err != nil {
+		return "", err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	return election.super.SendTransaction(transaction)
+}
+
+//"unregisterWitness","inputs":[],"outputs":[]
+func(election *Election) UnRegisterWitness(from common.Address) (string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("unregisterWitness")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	return election.super.SendTransaction(transaction)
+}
+
+//"queryCandidates","inputs":[{"name":"candidateAddress","type":"string"}],"outputs":[{"name":"owner","type":"string"},{"name":"voteCount","type":"uint64"},{"name":"active","type":"bool"},{"name":"url","type":"string"},{"name":"totalBounty","type":"uint64"},{"name":"extractedBounty","type":"uint64"},{"name":"lastExtractTime","type":"uint64"},{"name":"website","type":"string"},{"name":"name","type":"string"}]
+func(election *Election) GetCandidates(from common.Address, candidateAddress string)(*candidateInfo, error){
+	// encoding
+	inputEncode, err := election.abi.Pack("queryCandidates", candidateAddress)
+	if err != nil{
+		return nil, err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	requestResult, err := election.super.Call(transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//fmt.Println("result is ", requestResult.Result.(string))
+	var candidate candidateInfo
+	err = election.abi.Methods["queryCandidates"].Outputs.Unpack(&candidate, common.FromHex(requestResult.Result.(string)))
+	// 解码不应该出错，除非底层逻辑变更
+	if err != nil {
+		return nil, err
+	}
+
+	return &candidate, err
+}
+
+//"queryAllCandidates","inputs":[],"outputs":[{"name":"listNum","type":"uint64"},{"name":"candidateInfoList","type":"string"}]
+func(election *Election) GetAllCandidates(from common.Address)(*candidates, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("queryAllCandidates")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	requestResult, err := election.super.Call(transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//fmt.Println("result is ", requestResult.Result.(string))
+	var candidatesParse candidatesParsing
+	err = election.abi.Methods["queryAllCandidates"].Outputs.Unpack(&candidatesParse, common.FromHex(requestResult.Result.(string)))
+	// 解码不应该出错，除非底层逻辑变更
+	if err != nil {
+		return nil, err
+	}
+
+	var candidates candidates
+	for i := uint64(0);i<candidatesParse.ListNum;i++{
+		candidates.CandidatesList = append(candidates.CandidatesList, candidatesParse.CandidateInfoList[i*32:(i+1)*32-1])
+	}
+	candidates.Num = candidatesParse.ListNum
+	return &candidates, err
+}
+
+//"voteWitnesses","inputs":[{"name":"candidate","type":"string"}],"outputs":[]
+func(election *Election) VoteWitnesses(from common.Address, candidate string)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("voteWitnesses", candidate)
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"cancelVote","inputs":[],"outputs":[]
+func(election *Election) CancelVote(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("cancelVote")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"startProxy","inputs":[],"outputs":[]
+func(election *Election) StartProxy(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("startProxy")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"stopProxy","inputs":[],"outputs":[]
+func(election *Election) StopProxy(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("stopProxy")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"cancelProxy","inputs":[],"outputs":[]
+func(election *Election) CancelProxy(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("cancelProxy")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"setProxy","inputs":[{"name":"proxy","type":"string"}],"outputs":[]
+func(election *Election) SetProxy(from common.Address, proxy string)(string, error){
+	// encoding
+	inputEncode, err := election.abi.Pack("setProxy", proxy)
+	if err!= nil{
+		return "", err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"stake","inputs":[{"name":"stakeCount","type":"uint256"}],"outputs":[]
+func(election *Election) Stake(from common.Address, stakeCount *big.Int)(string, error){
+	// encoding
+	inputEncode, err := election.abi.Pack("stake", stakeCount)
+	if err!= nil{
+		return "", err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"unStake","inputs":[],"outputs":[]
+func(election *Election) UnStake(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("unStake")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"queryStake","inputs":[{"name":"voterAddress","type":"string"}],"outputs":[{"name":"owner","type":"string"},{"name":"stakeCount","type":"uint64"},{"name":"timestamp","type":"uint64"}]
+func(election *Election) GetStake(from common.Address, voterAddress string)(*stake, error){
+	// encoding
+	inputEncode, err := election.abi.Pack("queryStake", voterAddress)
+	if err != nil{
+		return nil, err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	requestResult, err := election.super.Call(transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//fmt.Println("result is ", requestResult.Result.(string))
+	var stakeInfo stake
+	err = election.abi.Methods["queryStake"].Outputs.Unpack(&stakeInfo, common.FromHex(requestResult.Result.(string)))
+	// 解码不应该出错，除非底层逻辑变更
+	if err != nil {
+		return nil, err
+	}
+
+	return &stakeInfo, err
+}
+
+//"extractOwnBounty","inputs":[],"outputs":[]
+func(election *Election) ExtractOwnBounty(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("extractOwnBounty")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//"issueAdditionalBounty","inputs":[],"outputs":[]
+func(election *Election) IssueAdditionalBounty(from common.Address)(string, error){
+	// encoding
+	inputEncode, _ := election.abi.Pack("issueAdditionalBounty")
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+
+	return election.super.SendTransaction(transaction)
+}
+
+//queryVoter","inputs":[{"name":"voterAddress","type":"string"}],"outputs":[{"name":"owner","type":"string"},{"name":"isProxy","type":"bool"},{"name":"proxyVoteCount","type":"uint64"},{"name":"proxy","type":"string"},{"name":"lastVoteCount","type":"uint64"},{"name":"timestamp","type":"uint64"},{"name":"voteCandidatesList","type":"string"}]
+func(election *Election) GetVoter(from common.Address, voterAddress string)(*voter, error){
+	// encoding
+	inputEncode, err := election.abi.Pack("queryVoter", voterAddress)
+	if err != nil{
+		return nil, err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	requestResult, err := election.super.Call(transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//fmt.Println("result is ", requestResult.Result.(string))
+	var voterInfo voter
+	err = election.abi.Methods["queryVoter"].Outputs.Unpack(&voterInfo, common.FromHex(requestResult.Result.(string)))
+	// 解码不应该出错，除非底层逻辑变更
+	if err != nil {
+		return nil, err
+	}
+
+	return &voterInfo, err
+}
+
+//"queryVoterList","inputs":[{"name":"candidateAddress","type":"string"}],"outputs":[{"name":"candidateAddress","type":"string"},{"name":"voterNum","type":"uint64"},{"name":"voterList","type":"string"}]
+func(election *Election) GetVoterList(from common.Address, candidateAddress string)(*Voters, error){
+	// encoding
+	inputEncode, err := election.abi.Pack("queryVoterList", candidateAddress)
+	if err != nil{
+		return nil, err
+	}
+
+	transaction := election.super.PrePareTransaction(from, ElectionContractAddr, types.ComplexString(hexutil.Encode(inputEncode)))
+	requestResult, err := election.super.Call(transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//fmt.Println("result is ", requestResult.Result.(string))
+	var voterList votersParsing
+	err = election.abi.Methods["queryVoterList"].Outputs.Unpack(&voterList, common.FromHex(requestResult.Result.(string)))
+	// 解码不应该出错，除非底层逻辑变更
+	if err != nil {
+		return nil, err
+	}
+
+	var voters Voters
+	for i := uint64(0);i<voterList.VoterNum;i++{
+		voters.VotersLi = append(voters.VotersLi, voterList.VoterList[i*32:(i+1)*32-1])
+	}
+
+	voters.CandidateAddress = voterList.CandidateAddress
+	voters.VoterNum = voterList.VoterNum
+	return &voters, err
+}
---
+++ test/System/election_test.go
@@ -0,0 +1,395 @@
+package System
+
+import (
+	"fmt"
+	"github.com/bif/bif-sdk-go"
+	"github.com/bif/bif-sdk-go/common"
+	"github.com/bif/bif-sdk-go/providers"
+	"github.com/bif/bif-sdk-go/system"
+	"github.com/bif/bif-sdk-go/test/resources"
+	"math/big"
+	"testing"
+)
+
+// 注册成为候选者节点
+func TestRegisterWitness(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+	elect := connection.System.NewElection()
+
+	registerWitness := new(system.RegisterWitness)
+	registerWitness.NodeUrl = "127.0.0.1/test"
+	registerWitness.Website = "www.tele.info.com"
+	registerWitness.Name = "BeiJing"
+
+	//registerWitness
+	registerWitnessHash, err := elect.RegisterWitness(common.StringToAddress(coinBase), registerWitness)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(registerWitnessHash, err)
+}
+
+// 取消成为候选者节点
+func TestUnRegisterWitness(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+	elect := connection.System.NewElection()
+
+	unRegisterWitnessHash, err := elect.UnRegisterWitness(common.StringToAddress(coinBase))
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(unRegisterWitnessHash, err)
+}
+
+// 获取候选节点基本信息
+func TestGetCandidates(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP52+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	candidate, err := elect.GetCandidates(common.StringToAddress(coinBase), coinBase)
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(fmt.Sprintf("%#v \n", candidate))
+	fmt.Println(uint(0x1e980), uint(0xea60), uint(0x1a1))
+//	0x1e980  0xea60 0x1a1
+}
+
+//获取所有的见证候选节点
+func TestGetAllCandidates(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	allCandidates, err := elect.GetAllCandidates(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(fmt.Sprintf("%#v \n", allCandidates))
+	//"did:bid:6cc796b8d6e2fbebc9b3cf9",
+	//"did:bid:13803fb30b7e95d57103c2d",
+	//"did:bid:c117c1794fc7a27bd301ae5",
+	//"did:bid:590ed37615bdfefa496224c",
+	//"did:bid:2e4f6a140ed099d15177d32",
+	//"did:bid:136993a53a0f3b4e5b5dc0d",
+	//"did:bid:55460d49c4364da777f6170",
+	//"did:bid:d8e55affc8ac5016185ddc6",
+	//"did:bid:0fcb3f265375cea7a57cceb",
+	//"did:bid:ade588198aeb5991541c850",
+	//"did:bid:c935bd29a90fbeea87badf3",
+	//"did:bid:cd68ab895f5a238ce60b98f",
+	//"did:bid:ee4f5aa444dc5ec815a25c7",
+	//"did:bid:d423816e6984eaa9bafb4bb",
+	//"did:bid:ae05edab896300f583747fd"
+}
+
+func TestVoteWitnesses(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP52+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	elect := connection.System.NewElection()
+
+	candidate := "did:bid:ee4f5aa444dc5ec815a25c7"
+	voteWitnessHash, err := elect.VoteWitnesses(common.StringToAddress(coinBase), candidate)
+	if err != nil && err != system.ErrCertificateNotExist {
+		t.Error(err)
+		t.FailNow()
+	}
+	if err == system.ErrCertificateNotExist {
+		t.Log(err)
+	}
+	t.Log(voteWitnessHash)
+}
+
+func TestElectCancelVote(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP52+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	cancelVoteHash, err := elect.CancelVote(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(cancelVoteHash)
+}
+
+func TestStartProxy(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	startProxyHash, err := elect.StartProxy(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(startProxyHash)
+}
+
+func TestStopProxy(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	stopProxyHash, err := elect.StopProxy(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(stopProxyHash)
+}
+
+//not set proxy
+func TestCancelProxy(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	cancelProxyHash, err := elect.CancelProxy(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(cancelProxyHash)
+}
+
+//account registered as a proxy is not allowed to use a proxy??
+func TestSetProxy(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	proxy := resources.Address51
+	setProxyHash, err := elect.SetProxy(common.StringToAddress(coinBase), proxy)
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(setProxyHash)
+}
+
+//权益抵押
+func TestStake(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	stakeHash, err := elect.Stake(common.StringToAddress(coinBase), big.NewInt(20))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(stakeHash)
+}
+
+// 权益抵押撤销
+func TestUnStake(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	unStakeHash, err := elect.UnStake(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(unStakeHash)
+}
+
+// 获取某个地址的权益抵押信息
+func TestGetStake(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP52+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	stake, err := elect.GetStake(common.StringToAddress(coinBase), coinBase)
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(fmt.Sprintf("%#v \n", stake))
+	//{Owner:"did:bid:c935bd29a90fbeea87badf3e", StakeCount:0xea60, Timestamp:0x5f0d2529}
+	//{Owner:"did:bid:590ed37615bdfefa496224c7", StakeCount:0xea60, Timestamp:0x5ef5b639}
+	//{Owner:"did:bid:6cc796b8d6e2fbebc9b3cf9e", StakeCount:0xea60, Timestamp:0x5ef5b55d}
+	//{Owner:"did:bid:13803fb30b7e95d57103c2dc", StakeCount:0xea60, Timestamp:0x5ef5b5f5}
+	//{Owner:"did:bid:c117c1794fc7a27bd301ae52", StakeCount:0xea60, Timestamp:0x5ef5b619}
+}
+
+func TestElectionExtractOwnBounty(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	extractHash, err := elect.ExtractOwnBounty(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(extractHash)
+}
+
+//？？？？？？？？？？？这个没有输出日志吗？？？？？
+func TestIssueAdditionalBounty(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	issueAdditionalBountyHash, err := elect.IssueAdditionalBounty(common.StringToAddress(coinBase))
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(issueAdditionalBountyHash)
+}
+
+// 获取指定候选者的基本信息
+func TestElectGetVoter(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	voter, err := elect.GetVoter(common.StringToAddress(coinBase),coinBase)
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(fmt.Sprintf("%#v \n", voter))
+//{Owner:"did:bid:c935bd29a90fbeea87badf3e", IsProxy:true, ProxyVoteCount:0x0, Proxy:"0000000000000000000000000000000000000000", LastVoteCount:0x0, Timestamp:0x5f0d20f9, VoteCandidatesList:""}
+//{Owner:"did:bid:590ed37615bdfefa496224c7", IsProxy:false, ProxyVoteCount:0x0, Proxy:"0000000000000000000000000000000000000000", LastVoteCount:0x1e7df, Timestamp:0x5ef5b6a9, VoteCandidatesList:"did:bid:590ed37615bdfefa496224c7"}
+//{Owner:"did:bid:6cc796b8d6e2fbebc9b3cf9e", IsProxy:false, ProxyVoteCount:0x0, Proxy:"0000000000000000000000000000000000000000", LastVoteCount:0x1e7df, Timestamp:0x5ef5b735, VoteCandidatesList:"did:bid:6cc796b8d6e2fbebc9b3cf9e"}
+//{Owner:"did:bid:13803fb30b7e95d57103c2dc", IsProxy:false, ProxyVoteCount:0x0, Proxy:"0000000000000000000000000000000000000000", LastVoteCount:0x1e7df, Timestamp:0x5ef5b6f1, VoteCandidatesList:"did:bid:13803fb30b7e95d57103c2dc"}
+//{Owner:"did:bid:c117c1794fc7a27bd301ae52", IsProxy:false, ProxyVoteCount:0x0, Proxy:"0000000000000000000000000000000000000000", LastVoteCount:0x1e7df, Timestamp:0x5ef5b6d1, VoteCandidatesList:"did:bid:c117c1794fc7a27bd301ae52"}
+
+}
+
+// 获取指定的候选者的投票列表
+func TestGetVoterList(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	coinBase, err := connection.Core.GetCoinbase()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	elect := connection.System.NewElection()
+
+	voterList, err := elect.GetVoterList(common.StringToAddress(coinBase), resources.Address52)
+
+	if err != nil{
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(fmt.Sprintf("%#v \n", voterList))
+	t.Log(voterList.VoterNum)
+
+}
