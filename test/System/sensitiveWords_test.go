9dd0b4daf5f3b5602f35dc51503894de8afb2afa
yashuoJin <j203025923@163.com>
2020-08-06 19:16:39 +0800

    add

---
+++ test/System/sensitiveWords_test.go
@@ -0,0 +1,155 @@
+package System
+
+import (
+	"github.com/bif/bif-sdk-go"
+	"github.com/bif/bif-sdk-go/core/block"
+	"github.com/bif/bif-sdk-go/providers"
+	"github.com/bif/bif-sdk-go/system"
+	"github.com/bif/bif-sdk-go/test/resources"
+	"math/big"
+	"testing"
+)
+
+func TestAddWord(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	chainId, err := connection.Core.GetChainId()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	nonce, err := connection.Core.GetTransactionCount(testAddress, block.LATEST)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	sysTxParams := new(system.SysTxParams)
+	sysTxParams.IsSM2 = isSM2
+	sysTxParams.Password = password
+	sysTxParams.KeyFileData = keyFileData
+	sysTxParams.GasPrice = big.NewInt(35)
+	sysTxParams.Gas = 2000000
+	sysTxParams.Nonce = nonce.Uint64()
+	sysTxParams.ChainId = big.NewInt(0).SetUint64(chainId)
+
+	sen := connection.System.NewSensitiveWord()
+
+	addWord := "北京"
+
+	txHash, err := sen.AddWord(sysTxParams, addWord)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(txHash)
+}
+
+func TestAddWords(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	chainId, err := connection.Core.GetChainId()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	nonce, err := connection.Core.GetTransactionCount(testAddress, block.LATEST)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	sysTxParams := new(system.SysTxParams)
+	sysTxParams.IsSM2 = isSM2
+	sysTxParams.Password = password
+	sysTxParams.KeyFileData = keyFileData
+	sysTxParams.GasPrice = big.NewInt(35)
+	sysTxParams.Gas = 2000000
+	sysTxParams.Nonce = nonce.Uint64()
+	sysTxParams.ChainId = big.NewInt(0).SetUint64(chainId)
+
+	sen := connection.System.NewSensitiveWord()
+
+	wordsLi := []string{"北京", "上海"}
+
+	txHash, err := sen.AddWords(sysTxParams, wordsLi)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(txHash)
+}
+
+func TestDelWord(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	chainId, err := connection.Core.GetChainId()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	nonce, err := connection.Core.GetTransactionCount(testAddress, block.LATEST)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	sysTxParams := new(system.SysTxParams)
+	sysTxParams.IsSM2 = isSM2
+	sysTxParams.Password = password
+	sysTxParams.KeyFileData = keyFileData
+	sysTxParams.GasPrice = big.NewInt(35)
+	sysTxParams.Gas = 2000000
+	sysTxParams.Nonce = nonce.Uint64()
+	sysTxParams.ChainId = big.NewInt(0).SetUint64(chainId)
+
+	sen := connection.System.NewSensitiveWord()
+
+	delWord := "北京"
+
+	txHash, err := sen.DelWord(sysTxParams, delWord)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(txHash)
+}
+
+func TestGetAllWords(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	_, err := connection.Core.GetCoinBase()
+
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	sen := connection.System.NewSensitiveWord()
+	wordsLi, err := sen.GetAllWords()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(wordsLi)
+}
+
+func TestIsContainWord(t *testing.T) {
+	var connection = bif.NewBif(providers.NewHTTPProvider(resources.IP+":"+resources.Port, 10, false))
+	_, err := connection.Core.GetCoinBase()
+
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	sen := connection.System.NewSensitiveWord()
+	word := "北京"
+	isContain, err := sen.IsContainWord(word)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(isContain)
+}
